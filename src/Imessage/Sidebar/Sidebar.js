cc6984fa4fe951af1fe73e8615ac554f6f33bbe5
maulibhalani12 <maulipatel12@outlook.com>
2021-03-01 22:19:07 +0530

    create sidebar header

---
+++ src/Imessage/Sidebar/Sidebar.js
@@ -0,0 +1,27 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import React from 'react';
+import "./Sidebar.css";
+import SearchIcon from '@material-ui/icons/Search';
+import RateReviewOutlinedIcon from '@material-ui/icons/RateReviewOutlined';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+
+            <div className="sidebar_header">
+                <Avatar className="sidebar_avatar" />
+                <div className="sidebar_input">
+                    <SearchIcon />
+                    <input placeholder="Search" />
+                </div>
+                <IconButton className="sidebar_inputButton">
+                    <RateReviewOutlinedIcon />
+                </IconButton>
+            </div>
+
+            <div className="sidebar_chats"></div>
+        </div>
+    );
+}
+
+export default Sidebar;
