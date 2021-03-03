098531a4c359a005372ad7ab109d6768aa8aa892
maulibhalani12 <maulipatel12@outlook.com>
2021-03-03 22:40:47 +0530

    create chat foot and login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { selectUser, login, logout } from './features/userSlice';
 import Imessage from './Imessage/Imessage';
+import Login from './Login/Login';
+import { auth } from "./firebase";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        // user is logged in
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        // user is logged out
+        dispatch(logout());
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
-      <Imessage />     
+      {user ? <Imessage /> : <Login />}     
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyBd8Y7RKo7uDDkPRNQo83CHUKR7KtBv16U",
     authDomain: "imessage-clone-b1bed.firebaseapp.com",
@@ -7,3 +9,11 @@ const firebaseConfig = {
     appId: "1:598675803890:web:b08ab880d753351b4a0a6b",
     measurementId: "G-Q5P7H58L1Y"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
