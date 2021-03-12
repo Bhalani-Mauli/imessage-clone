f3c97ead48b3286fd19eb609f4f53998b19f3ec9
maulibhalani12 <maulipatel12@outlook.com>
2021-03-12 16:32:30 +0530

    completed

--- src/Imessage/Sidebar/SidebarChat.js
+++ src/Imessage/Sidebar/SidebarChat.js
@@ -1,18 +1,50 @@
-import { Avatar } from '@material-ui/core';
-import React from 'react';
+import { Avatar } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setChat } from "../../features/chatSlice";
 import "./SidebarChat.css";
+import db from "../../firebase";
+import * as timeago from "timeago.js"
 
-function SidebarChat() {
+function SidebarChat({ id, chatName }) {
+    const dispatch = useDispatch();
+    const [chatInfo, setChatInfo] = useState([]);
+
+    useEffect(() => {
+        db.collection("chats")
+        .doc(id)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .onSnapshot(snapshot => (
+            setChatInfo(snapshot.docs.map(doc => doc.data()))
+        ));
+    }, [id]);
+
     return (
-        <div className="sidebarChat">
-            <Avatar />
+        <div
+            onClick={() => {
+                console.log(dispatch);
+                return dispatch(
+                    setChat({
+                        chatId: id,
+                        chatName: chatName,
+                    })
+                )
+            }
+            }
+            className="sidebarChat"
+        >
+            <Avatar src={chatInfo[0]?.photo} />
             <div className="sidebarChat_info">
-                <h2>Channel Name</h2>
-                <p>Last message sent...</p>
-                <small>timestamp</small>
+                <h2>{chatName}</h2>
+                <p>{chatInfo[0]?.message}</p>
+                <small>
+                    {timeago.format(
+                        new Date(chatInfo[0]?.timestamp?.toDate()))}
+                </small>
             </div>
         </div>
-    )
+    );
 }
 
 export default SidebarChat;
